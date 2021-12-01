9590319835ada65a397a4a2bdd389c3dc80baa0b
Najmul Huda <najmul.myself@gmail.com>
2021-12-01 13:24:48 +0600

    list view builder data added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +11,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List user = [
+      {"name": "Abid", "age": 22, "email": "abid@gmail.com"},
+      {"name": "Sabit", "age": 24, "email": "sabit@gmail.com"},
+      {"name": "Hasan", "age": 32, "email": "HasanKhan@gmail.com"},
+      {"name": "Arif", "age": 35, "email": "Arif@gmail.com"},
+      {"name": "Alamin", "age": 29, "email": "Alamin@gmail.com"},
+      {"name": "Sagor", "age": 40, "email": "Sagor@gmail.com"},
+      {"name": "Simanto", "age": 23, "email": "Simanto@gmail.com"},
+      {"name": "Robin", "age": 25, "email": "Robin@gmail.com"},
+      {"name": "Firoj", "age": 26, "email": "firoj@gmail.com"},
+      {"name": "Mohshin", "age": 25, "email": "Mohshin@gmail.com"},
+      {"name": "Ronjon", "age": 53, "email": "ronjon@gmail.com"},
+      {"name": "khaled", "age": 31, "email": "khaled@gmail.com"},
+      {"name": "Saikat", "age": 36, "email": "Saikat@gmail.com"},
+    ];
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text("List View"),
@@ -18,7 +36,7 @@ class MyApp extends StatelessWidget {
           width: double.infinity,
           height: double.infinity,
           child: ListView.builder(
-            itemCount: 20,
+            itemCount: user.length,
             itemBuilder: (context, i) {
               return Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -26,6 +44,40 @@ class MyApp extends StatelessWidget {
                   width: double.infinity,
                   height: 180,
                   color: Colors.redAccent,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Profile of ${user[i]["name"]}",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                          "-------------------",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        Text(
+                          "Name : ${user[i]['name']}",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                          "Name : ${user[i]['age']}",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                          "Name : ${user[i]['email']}",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                          "-------------------",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               );
             },
